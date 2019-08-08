cc5f1958974e7d2b803a0f0f9f91182f30c1ddbd
adkarras <chw111111@hotmail.com>
2019-08-08 00:17:08 -0600

    Add geocoding API

--- Pages/WeatherController.cs
+++ Pages/WeatherController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Geocoding;
+using Geocoding.Google;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,6 +20,15 @@ namespace WeatherIsFun.Pages
             ApiClass api = new ApiClass();
             api.getWeatherByCityName("Layton", "UT");
             return state + city;
+        }
+
+        [HttpGet("{address}", Name = "Get")]
+        public async Task<string> Get(string address)
+        {
+            IGeocoder geocoder = new GoogleGeocoder() { ApiKey = "AIzaSyAZU4WGweBjNGS3l-a_i81SlD2OtgqggSw" };
+            IEnumerable<Address> addresses = await geocoder.GeocodeAsync(address);
+            
+            return "";
         }
     }
 }
